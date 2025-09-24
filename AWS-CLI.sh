26f4b126a34e9ca6055d58fbe33fa19697f43f24
kriiishamtic <kriiishmatic@gmail.com>
2025-09-24 14:22:13 +0530

    robo

--- AWS-CLI.sh
+++ AWS-CLI.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-05e5bd14656ebdaf3"
+Zone_ID="Z0212707MY585LOOEFGA"
+Domain="kriiishmatic.fun"
 
 for instance in $@
     do
@@ -10,11 +12,30 @@ for instance in $@
     if [ $instance != "frontend" ]; then
 
         IP=$(aws ec2 describe-instances --instance-ids $INSTA_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text )
-
+        Final="$instance.$Domain" # other ec2 
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTA_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text	)
+        Final=".$Domain" #fronend
     fi
 
     echo " $instance=$IP "
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$Final'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
 
     done 
