4f538980a49fbc2495ac78a6a42dc3c6a6d17306
kriiishamtic <kriiishmatic@gmail.com>
2025-09-26 16:02:49 +0530

    add

---
+++ 04-catalogue.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#colours
+#####################
+
+mongodIP="mongodb.kriiishmatic.fun"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo -e " $R Get sudo access BOZO $N "
+    exit 3
+fi
+
+
+#creating logs files
+####################
+
+shell_log="/var/log/robo-project"
+
+mkdir -p $shell_log
+
+#removing .sh from file
+#######################
+
+Remove_sh=$( echo $0 | cut -d "." -f1 )
+
+Logfile="$shell_log/$Remove_sh.log"
+DIR=$(pwd)
+
+echo " Script started at :: $(date) " | tee -a $Logfile
+
+
+Status(){
+    if [ $1 -ne 0 ]; then
+    echo -e " $R Failed $N  $2  " | tee -a $Logfile
+    exit 1
+else
+    echo -e "$G Sucessfully $N $2 " | tee -a $Logfile
+fi
+}
+
+####### NODEJS ######
+
+dnf module disable nodejs -y &>>$Logfile
+Status $? "Disabled the node"
+
+dnf module enable nodejs:20 -y 
+Status $? "enabled the node"
+dnf install nodejs -y &>>$Logfile
+Status $? "installed the node"
+
+id roboshop &>>$Logfile
+if [ $? -ne 0 ]; then
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    else
+    echo -e " Already installed $Y SKIPPED! $N "
+fi
+
+mkdir -p /app 
+Status $? "Creating directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
+Status $? "Dowloadting code zip"
+
+
+cd /app 
+rm -r /app*
+Status $? "Removed previous code"
+
+unzip /tmp/catalogue.zip
+Status $? "unzipping latest code"
+
+cd /app 
+
+npm install &>>$Logfile
+Status $? "installed the DEPENDENCIES"
+
+cp $DIR/catalogue.service /etc/systemd/system/catalogue.service
+Status $? "Copied catalogue service"
+
+systemctl daemon-reload
+Status $? "Demonic reload"
+
+systemctl enable catalogue &>>$Logfile
+Status $? "Enabled the node"
+
+systemctl start catalogue &>>$Logfile
+Status $? "Start it "
+
+cp $DIR/mongo.rep /etc/yum.repos.d/mongo.repo
+Status $? "Mongo repo is here"
+
+dnf install mongodb-mongosh -y &>>$Logfile
+Status $? "installed the mongoin in catalogue "
+
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
+mongosh --host $mongodIP </app/db/master-data.js
+    else
+echo -e " Already loaded products and masterdata so $Y skipping $N "
+fi
+
+
+dnf restart catalogue.service &>>$Logfile
+Status $? "Restarted the service"
+
+echo -e " Loading products into DB and $G restarted the catalogue $N "
+
+telnet -lntp 
+Status $? "Checking the Ports"
+
+http://localhealth:8080/
+Status $? "Node health"
+
+
